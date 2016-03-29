1ad5200fb52fb6776a1eaf80591470202a005ebb
user <nobody>
2016-03-29 02:08:29 +0000

    auto (/extra/home/packages/git/dlang/htmld/src/html/escape.d) 77 1486

--- src/html/escape.d
+++ src/html/escape.d
@@ -5,17 +5,31 @@ enum HTMLEscapes {
 
 enum AttributeEscapes {
   quot = '"',
-  
-
-string checkEscapes() {
-  return q{
-	if(chunk.startsWith("%1$s;")) {
-	  result.put("%2$s");
-	  result.put(chunk["%1$s;".length:$]);
-	} else
-  }.format(match,replacement);
+  apos = '\'',
 }
-	  
+
+string checkEnum(alias Escapes, bool escape = true)(string s) {
+  for(e;EnumMembers!Escapes) {
+	static if(escape) {
+	  app.put(q{
+		  if(chunk.startsWith("%1$s")) {
+			result.put("&%2$s;");
+			result.put(chunk["%1$s".length:$]);
+			continue;
+		  }
+		}.format(to!wchar(e),to!string(e)));
+	} else {
+	  app.put(q{
+		  if(chunk.startsWith("%1$s;")) {
+			result.put("%2$s");
+			result.put(chunk["%1$s;".length:$]);
+			continue;
+		  }
+		}.format(to!string(e),to!wchar(e)));
+	}
+  }
+  return app.data;
+}
 
 auto unescape(bool unicode = true,
 			  bool html = false,
@@ -26,12 +40,57 @@ auto unescape(bool unicode = true,
   if(chunks.empty) return s;
   chunks.popFront();
   if(chunks.empty) return s;
-  
+
   foreach(chunk; chunks) {
-	mixin checkEscape("lt")
-	  mixin checkEscape("gt")
-	  mixin checkEscape("
+	if(chunk.length == 0) {
+	  app.put("&");
+	  continue;
+	}
 	static if(html) {
-	  if(chunk.startsWith("lt;"
-	  
-	
+	  mixin checkEscapes!(HTMLEscapes,false);
+	}
+	static if(attribute) {
+	  mixin checkEscapes!(AttributeEscapes,false);
+	}
+	static if(unicode) {
+	  mixin checkEscapes!(NamedEscapes, false);
+	  switch(chunk[0]) {
+	  case '#':
+		auto pos = chunk.countUntil(';');
+		if(pos < 0) {
+		  app.put("&" ~ chunk);
+		  continue;
+		}
+		auto num = chunk[1..pos];
+		if(!isNumeric(num)) {
+		  app.put("&" ~ chunk);
+		  continue;
+		}
+		app.put(num.to!int.to!dchar.to!string);
+		app.put(chunk[pos..$]);
+		continue;
+	  }
+	case 'x':
+	  auto pos = chunk.countUntil(';');
+	  if(pos < 0) {
+		app.put("&" ~ chunk);
+		continue;
+	  }
+	  auto num = chunk[1..pos];
+	  if(num.count("abcdefABCDEF0123456789") < num.length) {
+		app.put("&" ~ chunk);
+		continue;
+	  }
+	  app.put(to!int(num,0x10).to!dchar.to!string);
+	default:
+	  app.put("&" ~ chunk);
+	  continue;
+	}
+  }
+  return app.data;
+}
+
+unittest {
+  import std.stdio;
+  writeln("hmmm ",unescape("&lt;html&gt;&amp;amp; &#160; &x37;"));
+}
