930b76217dd287b863da14c5398c878907c0d6a7
user <nobody>
2016-03-29 02:14:45 +0000

    auto (/extra/home/packages/git/dlang/htmld/src/html/escape.d) 52 1309

--- src/html/escape.d
+++ src/html/escape.d
@@ -1,3 +1,7 @@
+module html.escape;
+
+import html: HTMLString;
+
 enum HTMLEscapes {
   lt = '<',
   gt = '>',
@@ -8,21 +12,30 @@ enum AttributeEscapes {
   apos = '\'',
 }
 
+enum NamedEscapes {
+  nbsp = ' '
+}
+
 string checkEnum(alias Escapes, bool escape = true)(string s) {
-  for(e;EnumMembers!Escapes) {
+  import std.array: appender;
+  import std.traits: EnumMembers;
+  import std.format: format;
+  import std.conv: to;
+  auto app = appender!string;
+  foreach(e;EnumMembers!Escapes) {
 	static if(escape) {
 	  app.put(q{
 		  if(chunk.startsWith("%1$s")) {
-			result.put("&%2$s;");
-			result.put(chunk["%1$s".length:$]);
+			app.put("&%2$s;");
+			app.put(chunk["%1$s".length:$]);
 			continue;
 		  }
 		}.format(to!wchar(e),to!string(e)));
 	} else {
 	  app.put(q{
 		  if(chunk.startsWith("%1$s;")) {
-			result.put("%2$s");
-			result.put(chunk["%1$s;".length:$]);
+			app.put("%2$s");
+			app.put(chunk["%1$s;".length:$]);
 			continue;
 		  }
 		}.format(to!string(e),to!wchar(e)));
@@ -34,8 +47,13 @@ string checkEnum(alias Escapes, bool escape = true)(string s) {
 auto unescape(bool unicode = true,
 			  bool html = false,
 			  bool attribute = false)
-  (HTMLstring s) {
-  appender!HTMLstring result;
+  (HTMLString s) {
+  import std.array: appender;
+  import std.algorithm.iteration: splitter;
+  import std.conv: to;
+  import std.string: isNumeric;
+  import std.algorithm.searching: countUntil, count;
+  auto app = appender!HTMLString;
   auto chunks = s.splitter("&");
   if(chunks.empty) return s;
   chunks.popFront();
@@ -47,13 +65,13 @@ auto unescape(bool unicode = true,
 	  continue;
 	}
 	static if(html) {
-	  mixin checkEscapes!(HTMLEscapes,false);
+	  mixin checkEnum!(HTMLEscapes,false);
 	}
 	static if(attribute) {
-	  mixin checkEscapes!(AttributeEscapes,false);
+	  mixin checkEnum!(AttributeEscapes,false);
 	}
 	static if(unicode) {
-	  mixin checkEscapes!(NamedEscapes, false);
+	  mixin checkEnum!(NamedEscapes, false);
 	  switch(chunk[0]) {
 	  case '#':
 		auto pos = chunk.countUntil(';');
@@ -69,22 +87,24 @@ auto unescape(bool unicode = true,
 		app.put(num.to!int.to!dchar.to!string);
 		app.put(chunk[pos..$]);
 		continue;
-	  }
-	case 'x':
-	  auto pos = chunk.countUntil(';');
-	  if(pos < 0) {
+
+	  case 'x':
+		auto pos = chunk.countUntil(';');
+		if(pos < 0) {
+		  app.put("&" ~ chunk);
+		  continue;
+		}
+		auto num = chunk[1..pos];
+		if(num.count("abcdefABCDEF0123456789") < num.length) {
+		  app.put("&" ~ chunk);
+		  continue;
+		}
+		app.put(to!int(num,0x10).to!dchar.to!string);
+		continue;
+	  default:
 		app.put("&" ~ chunk);
 		continue;
 	  }
-	  auto num = chunk[1..pos];
-	  if(num.count("abcdefABCDEF0123456789") < num.length) {
-		app.put("&" ~ chunk);
-		continue;
-	  }
-	  app.put(to!int(num,0x10).to!dchar.to!string);
-	default:
-	  app.put("&" ~ chunk);
-	  continue;
 	}
   }
   return app.data;
