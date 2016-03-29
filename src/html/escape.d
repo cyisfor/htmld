77d6af4e5344a57164d5cd6a1d3e95a9af57cff5
user <nobody>
2016-03-29 04:27:38 +0000

    auto (/extra/home/packages/git/dlang/htmld/src/html/escape.d) 71 1526

--- src/html/escape.d
+++ src/html/escape.d
@@ -24,7 +24,7 @@ import std.array: Appender;
 
 bool checkEnum(alias Escapes, bool escape = true)
   (ref HTMLString chunk,
-   Appender!HTMLString app) {
+   ref Appender!HTMLString app) {
   import std.string: startsWith;
   foreach(e;EnumMembers!Escapes) {
 	auto repr = e.to!dchar.to!string;
@@ -32,6 +32,7 @@ bool checkEnum(alias Escapes, bool escape = true)
 	if(repr == "\"")
 	  repr = "\\\"";
 	static if(escape == true) {
+	  writeln("hmm",repr," ",chunk);
 	  if(chunk.startsWith(repr)) {
 		app.put("&"~name~";");
 		app.put(chunk[repr.length..$]);
@@ -78,20 +79,20 @@ auto unescape(bool unicode = true,
 		case '#':
 		  auto pos = chunk[1..$].countUntil(';');
 		  if(pos < 0) return false;
-		  auto num = chunk[1..pos];
+		  auto num = chunk[1..pos+1];
 		  if(!isNumeric(num)) return false;
-		  
 		  app.put(num.to!int.to!dchar.to!string);
-		  app.put(chunk[pos..$]);
+		  app.put(chunk[pos+2..$]);
 		  return true;
 		case 'x':
 		  auto pos = chunk[1..$].countUntil(';');
 		  if(pos < 0) return false;
-		  auto num = chunk[1..pos];
+		  auto num = chunk[1..pos+1];
 		  if(num.count!((c) => 0 == "abcdefABCDEF0123456789".count(c)) > 0)
 			return false;
 		
 		  app.put(to!int(num,0x10).to!dchar.to!string);
+		  app.put(chunk[pos+2..$]);
 		  return true;
 		default:
 		  if(checkEnum!(NamedEscapes, false)(chunk,app)) return true;
@@ -110,76 +111,85 @@ auto unescape(bool unicode = true,
 unittest {
   import std.stdio;
   import std.algorithm.comparison: equal;
-  assert
-	(equal(unescape!(true,true,true)("&lt;html&gt;&amp;amp; &#160; &x37;"),
-		   `<html>&amp; 0; `));
+  void assert_equal(HTMLString a, HTMLString b) {
+	if(a != b) {
+	  writeln("fail");
+	  writeln(a);
+	  writeln(b);
+	  assert(false);
+	}
+  }
+  assert_equal
+	(unescape!(true,true,true)("&lt;html&gt;&amp;amp; &#42; &x42;"),
+	 "<html>&amp; * B");
 }
 
 auto escape(bool unicode = true,
 			  bool html = false,
 			  bool attribute = false)
   (HTMLString s) {
+  if(s.length == 0) return s;
+  
   import std.array: appender;
   import std.algorithm.iteration: splitter;
   import std.ascii: isPrintable;
-  auto app = appender!HTMLString;
-  bool criteria(const(char) c) {
+
+  bool criteria(const(dchar) c) {
 	static if(unicode) {
 	  // break on utf-8 multibyte characters, or control characters.
 	  if(!isPrintable(c)) return true;
 	  foreach(e;EnumMembers!NamedEscapes) {
-		if(e.to!char == c) return true;
+		if(e.to!dchar == c) return true;
 	  }
 	}
 	static if(html) {
 	  foreach(e;EnumMembers!HTMLEscapes) {
-		if(e.to!char == c) return true;
+		if(e.to!dchar == c) return true;
 	  }
 	}
 	static if(attribute) {
 	  foreach(e;EnumMembers!AttributeEscapes) {
-		if(e.to!char == c) return true;
+		if(e.to!dchar == c) return true;
 	  }
 	}
 	return false;
   }
 
-  import std.range: isForwardRange;
-  import std.functional: unaryFun;
-  pragma(msg,typeid(typeof((cast(Range)s).front)))
-  static assert(isForwardRange!(typeof(s))
-				&& is(typeof(unaryFun!criteria((cast(Range)s).front))));
-  auto chunks = splitter!(criteria)(s);
-  if(chunks.empty) return s;
-  chunks.popFront();
-  if(chunks.empty) return s;
-  foreach(chunk; chunks) {
-	writeln("okay... ",chunk);
+  auto app = appender!HTMLString;
+
+  /* it's fastest to just iterate over the bytes...
+	 because we can't memchr since we don't know what we're looking for.
+  */
+  int last = 0;
+  for(int i=0;i<s.length;++i) {
+	if(!criteria(s[i])) continue;
+	if(i > last) {
+	  app.put(s[last..i-1]);
+	}
+	writeln("okay... ",s[i]);
 	bool handle() {
 	  static if(unicode) {
 		import std.utf: decode;
-		if(checkEnum!(NamedEscapes, true)(chunk,app)) return true;
-		if(chunk[0] > 0x7f) {
+		if(checkEnum!(NamedEscapes, true)(&s[i],app)) return true;
+		if(s[i] > 0x7f) {
 		  size_t num = 0;
-		  auto point = decode(chunk,num);
+		  auto point = decode(&s[i],num);
 		  app.put("&x");
 		  app.put(to!string(to!uint(point),0x10));
 		  app.put(";");
-		  app.put(chunk[num..$]);
 		  return true;
 		}
 	  }
 	  static if(html) {
-		if(checkEnum!(HTMLEscapes, true)(chunk,app)) return true;
+		if(checkEnum!(HTMLEscapes, true)(&s[i],app)) return true;
 	  }
 	  static if(attribute) {
-		if(checkEnum!(AttributeEscapes, true)(chunk,app)) return true;		
+		if(checkEnum!(AttributeEscapes, true)(&s[i],app)) return true;		
 	  }
 	  return false;
 	}
 	if(!handle()) {
-	  app.put('&');
-	  app.put(chunk);
+	  app.put(s[i]);
 	}
   }
   return app.data;
